246df91225b46b65953417c7f0b7c55103625427
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2021-11-22 18:01:12 -0300

    Working API with CRUD

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,26 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-const serviceAccount = require("./permissions.json");
+const serviceAccount = require('./permissions.json')
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://restapi-js.firebaseio.com",
-});
+})
 
-app.use(cors({ origin: true }));
+app.use(cors({ origin: true }))
 
-app.get("/hello-world", (req, res) => {
-  return res.status(200).json({ message: "Hello World!" });
-});
+app.get('/hello-world', (req, res) => {
+  return res.status(200).json({ message: 'Hello World!' })
+})
 
 // Routes
-app.use(require("./routes/products.routes"));
+app.use(require('./routes/products.routes'))
 
-exports.app = functions.https.onRequest(app);
+exports.app = functions.https.onRequest(app)
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
